ab3527bbae1dd2c8ce324b8b8bce532ea57eeba6
josuelandero13 <josue.landero130899@gmail.com>
2024-11-23 18:54:49 -0600

    - MVC architecture implementation

--- app.js
+++ app.js
@@ -1,112 +1,19 @@
-const express = require('express')
-const movies = require('./movies.json')
-const crypto = require('crypto')
-const cors = require('cors')
-const { validateMovie, validateMovieUpdated } = require('./schemas/movies.js')
+import express, { json } from "express";
+import { corsMiddleware } from "./middlewares/cors.js";
+import { moviesRouter } from "./routes/movies.js";
 
-const app = express()
-app.use(express.json())
-app.use(cors({
-  origin: (origin, callback) => {
-    const ACCEPTED_ORIGINS = [
-      'http://localhost:8080',
-      'http://localhost:1234',
-      'https://movies.com'
-    ]
+const app = express();
+app.disable("x-powered-by");
 
-    if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
-    }
+app.use(json());
+app.use(corsMiddleware());
 
-    if (!origin) {
-      return callback(null, true)
-    }
+app.use("/movies", moviesRouter);
 
-    return callback(new Error('Not allowed by CORS'))
-  }
-}))
-
-app.disable('x-powered-by')
-
-app.get('/movies', (req, res) => {
-  const { genre } = req.query
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
-    )
-    return res.json(filteredMovies)
-  }
-
-  res.json(movies)
-})
-
-app.get('/movies/:id', (req, res) => {
-  const { id } = req.params
-  const movie = movies.find((movie) => movie.id === id)
-
-  if (!movie) {
-    res.status(404).json({ error: 'Movie not found' })
-    return
-  }
-  res.json(movie)
-})
-
-app.post('/movies', (req, res) => {
-  const result = validateMovie(req.body)
-  if (!result.success) {
-    res.status(400).json({ error: JSON.parse(result.error.message) })
-    return
-  }
-
-  const newMovie = {
-    id: crypto.randomUUID(),
-    ...result.data
-  }
-
-  movies.push(newMovie)
-
-  res.status(201).json(movies)
-})
-
-app.delete('/movies/:id', (req, res) => {
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  movies.splice(movieIndex, 1)
-
-  return res.json({ message: 'Movie deleted' })
-})
-
-app.patch('/movies/:id', (req, res) => {
-  const result = validateMovieUpdated(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data
-  }
-
-  movies[movieIndex] = updateMovie
-
-  return res.json(updateMovie)
-})
-
-const PORT = process.env.PORT ?? 1234
+const PORT = process.env.PORT ?? 1234;
 
 app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`)
-})
+  console.log(`
+    Server listening on port http://localhost:${PORT}`
+  );
+});
--- schemas/movies.js
+++ schemas/movies.js
@@ -1,48 +1,46 @@
-const z = require('zod')
+import { z } from "zod";
 
 const movieSchema = z.object({
   title: z.string({
-    invalid_type_error: 'Title must be a string',
-    required_error: 'Title is required'
+    invalid_type_error: "Title must be a string",
+    required_error: "Title is required",
   }),
   year: z.number().int().positive().min(1900).max(2024),
   director: z.string(),
   duration: z.number().int().positive(),
   poster: z.string().url({
-    message: 'Poster must be a valid URL'
+    message: "Poster must be a valid URL",
   }),
   genre: z.array(
     z.enum(
       [
-        'Action',
-        'Adventure',
-        'Animation',
-        'Comedy',
-        'Documentary',
-        'Drama',
-        'Horror',
-        'Music',
-        'Mystery',
-        'Romance',
-        'Sci-Fi',
-        'Thriller',
-        'Crime'
+        "Action",
+        "Adventure",
+        "Animation",
+        "Comedy",
+        "Documentary",
+        "Drama",
+        "Horror",
+        "Music",
+        "Mystery",
+        "Romance",
+        "Sci-Fi",
+        "Thriller",
+        "Crime",
       ],
       {
-        required_error: 'Genre is required',
-        invalid_type_error: 'Rate must be a number'
+        required_error: "Genre is required",
+        invalid_type_error: "Rate must be a number",
       }
     )
   ),
-  rate: z.number().min(0).max(10).default(5)
-})
+  rate: z.number().min(0).max(10).default(5),
+});
 
-function validateMovie (object) {
-  return movieSchema.safeParse(object)
+export function validateMovie(object) {
+  return movieSchema.safeParse(object);
 }
 
-function validateMovieUpdated (shape) {
-  return movieSchema.partial().safeParse(shape)
+export function validateMovieUpdated(shape) {
+  return movieSchema.partial().safeParse(shape);
 }
-
-module.exports = { validateMovie, validateMovieUpdated }
